7873724bba676adb95cba60cd0c017b2e9b5a4a2
dharwee <dharweedubae@gmail.com>
2025-02-09 22:40:00 +0530

    adding-api

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,7 @@
+import "../css/Favorites.css"
 export default function favorites(){
     return(
-        <div className="favorite-empty">
+        <div className="favorites-empty">
             <h2>No favorite movies yet</h2>
             <p>Start adding movies to your favorites and they will appear here</p>
         </div>
